7a8d3bf74231b48ec9cd2fedf3c61577587933b6
Aquilesw567 <eldiademsufer666@gmail.com>
2026-03-29 18:14:50 -0500

    Create config.py with IDE theme and colors settings

---
+++ config.py
@@ -0,0 +1,9 @@
+# Configuration settings for IDE theme and colors
+
+# Theme settings
+IDE_THEME = 'Dark'
+
+# Color settings
+PRIMARY_COLOR = '#1E1E1E'
+SECONDARY_COLOR = '#F3F3F3'
+ACCENT_COLOR = '#007ACC'
