dddf6ac300304f0d02470d1af2d60eb539e33a82
Aquilesw567 <eldiademsufer666@gmail.com>
2026-03-29 18:59:46 -0500

    Create ui/explorer.py with FileExplorer class for file browsing

---
+++ ui/explorer.py
@@ -0,0 +1,15 @@
+class FileExplorer:
+    def __init__(self, root_path):
+        self.root_path = root_path
+
+    def browse(self):
+        # Logic to browse files in the root_path
+        pass
+
+    def open_file(self, file_path):
+        # Logic to open a file
+        pass
+
+    def list_files(self):
+        # Logic to list all files in the current directory
+        pass
