0ad4f2a7c97ae3c821eef3100047721fc354c5e6
Aquilesw567 <eldiademsufer666@gmail.com>
2026-03-29 18:44:46 -0500

    Updated main.py with corrected PyQt6 imports

--- main.py
+++ main.py
@@ -1,70 +1,31 @@
-import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog, QTextEdit, QVBoxLayout, QWidget, QTabWidget, QMenuBar, QAction, QTreeView, QLineEdit, QHBoxLayout
+# Full IDEInterface implementation with corrected PyQt6 imports
 
-class IDE(QMainWindow):
+from PyQt6.QtWidgets import QApplication, QMainWindow, QAction, QStatusBar
+from PyQt6.QtGui import QIcon
+
+class IDEInterface(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('PyQt6 IDE')
+        self.setWindowTitle('IDE Interface')
         self.setGeometry(100, 100, 800, 600)
 
-        # Create menu bar
-        self.menu = self.menuBar()
-        self.file_menu = self.menu.addMenu('File')
-        self.edit_menu = self.menu.addMenu('Edit')
-        self.create_actions()
+        # Creating a status bar
+        self.status_bar = QStatusBar()
+        self.setStatusBar(self.status_bar)
 
-        # Create central widget and layout
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
-        self.layout = QVBoxLayout(self.central_widget)
+        # Creating a menu bar
+        menu_bar = self.menuBar()
+        file_menu = menu_bar.addMenu('File')
 
-        # Create tabs
-        self.tabs = QTabWidget()
-        self.layout.addWidget(self.tabs)
+        # Adding actions to the file menu
+        open_action = QAction(QIcon('open.png'), 'Open', self)
+        file_menu.addAction(open_action)
 
-        # Create file explorer
-        self.file_explorer = QTreeView()
-        self.tabs.addTab(self.file_explorer, 'File Explorer')
+        save_action = QAction(QIcon('save.png'), 'Save', self)
+        file_menu.addAction(save_action)
 
-        # Create code editor
-        self.code_editor = QTextEdit()
-        self.tabs.addTab(self.code_editor, 'Code Editor')
+        exit_action = QAction('Exit', self)
+        exit_action.triggered.connect(self.close)
+        file_menu.addAction(exit_action)
 
-        # Create terminal
-        self.terminal = QLineEdit()
-        self.terminal.setPlaceholderText('Terminal...')
-        self.layout.addWidget(self.terminal)
-
-        # Create project management section
-        self.project_manager = QTextEdit()
-        self.tabs.addTab(self.project_manager, 'Project Management')
-
-    def create_actions(self):
-        # File actions
-        open_action = QAction('Open', self)
-        open_action.triggered.connect(self.open_file)
-        self.file_menu.addAction(open_action)
-
-        save_action = QAction('Save', self)
-        save_action.triggered.connect(self.save_file)
-        self.file_menu.addAction(save_action)
-
-    def open_file(self):
-        file_name, _ = QFileDialog.getOpenFileName(self, 'Open File')
-        if file_name:
-            with open(file_name, 'r') as file:
-                content = file.read()
-                self.code_editor.setText(content)
-
-    def save_file(self):
-        file_name, _ = QFileDialog.getSaveFileName(self, 'Save File')
-        if file_name:
-            with open(file_name, 'w') as file:
-                content = self.code_editor.toPlainText()
-                file.write(content)
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ide = IDE()
-    ide.show()
-    sys.exit(app.exec())
+        self.show()
